e2789347c63fb901bcfce735ff9ff23fa80d859a
Kasparas Karlauskas <121799419+kasparas-k@users.noreply.github.com>
2025-11-19 09:49:40 +0900

    Fix typo

--- python/pc_rwalker/pc_rwalker.cpp
+++ python/pc_rwalker/pc_rwalker.cpp
@@ -17,7 +17,7 @@ PYBIND11_MODULE(_pc_rwalker, m) {
         py::arg("seed_indices"),
         py::arg("n_neighbors"),
         py::arg("sigma1") = 1.0,
-        py::arg("simga2") = 1.0, 
+        py::arg("sigma2") = 1.0, 
         py::arg("min_weight") = 0.0001,
         py::arg("n_proc") = -1
     );
